4c93b040f66cafe5a872219c95385c09b0264849
Yimi Aaron Campos <yimic@clarochile.clarochile.org>
2020-12-21 13:23:09 -0300

    Ajuste de splashCreen

--- lib/tabs/splash_nuevo.dart
+++ lib/tabs/splash_nuevo.dart
@@ -11,13 +11,14 @@ class SplashNuevo extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
         statusBarColor: Colors.pink[900], //or set color with: Color(0xFF0000FF)
         systemNavigationBarColor: Colors.pink[900]));
-    return Scaffold(
-      body: InkWell(
-        child: Container(
-          child:
-              new Image.asset('lib/images/inicio-app.jpg', fit: BoxFit.cover),
-          alignment: Alignment.center,
+    return new Scaffold(
+      body: new Container(
+        child: new Image.asset(
+          'lib/images/inicio-app.jpg',
+          fit: BoxFit.fitHeight,
         ),
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.center,
       ),
     );
   }
